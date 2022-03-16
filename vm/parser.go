febecee19293063fe2ac3c9bd03126195b58196a
Patrick Brophy <patrick.brophy2@gmail.com>
2022-03-15 22:32:44 -0400

    formatting and a comment was incorrect lol

--- vm/parser.go
+++ vm/parser.go
@@ -19,88 +19,88 @@ DUMP
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
-  "strconv"
 )
 
 func Parse(path string) (Program, error) {
-  if len(path) == 0 {
-    return nil, fmt.Errorf("No path provided")
-  }
-  raw, err := os.ReadFile(path)
-  if err != nil {
-    return nil, err
-  }
+	if len(path) == 0 {
+		return nil, fmt.Errorf("No path provided")
+	}
+	raw, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
 
-  file := string(raw)
+	file := string(raw)
 
-  program := make(Program, 0)
-  lines := strings.Split(file, "\n")
+	program := make(Program, 0)
+	lines := strings.Split(file, "\n")
 
-  for _, line := range lines {
-    if len(line) == 0 {
-      continue
-    }
-    inst, err := ParseLine(line)
-    if err != nil {
-      return nil, err
-    }
-    program = append(program, inst)
-  }
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		inst, err := ParseLine(line)
+		if err != nil {
+			return nil, err
+		}
+		program = append(program, inst)
+	}
 
-  return program, nil
+	return program, nil
 }
 
 func ParseLine(line string) (Instruction, error) {
-  if len(line) == 0 {
-    return Instruction{}, fmt.Errorf("No line provided")
-  }
+	if len(line) == 0 {
+		return Instruction{}, fmt.Errorf("No line provided")
+	}
 
-  parts := strings.Split(line, " ")
-  if (len(parts) < 1) {
-    return Instruction{}, fmt.Errorf("No instruction provided")
-  }
+	parts := strings.Split(line, " ")
+	if len(parts) < 1 {
+		return Instruction{}, fmt.Errorf("No instruction provided")
+	}
 
-  // if the instruction is a PUSH or POP, we need to parse the operand and the data
-  if len(parts) > 1 {
-    op, err := ParseOperand(parts[0])
-    if err != nil {
-      return Instruction{}, err
-    }
-    data, err := strconv.Atoi(parts[1])
-    if err != nil {
-      return Instruction{}, err
-    }
-    return Instruction{op, data}, nil
-  }
-  // otherwise, we just need to parse the instruction
-  op, err := ParseOperand(parts[0])
-  if err != nil {
-    return Instruction{}, err
-  }
+	// if the instruction is a PUSH we need to parse the operand and the data
+	if len(parts) > 1 {
+		op, err := ParseOperand(parts[0])
+		if err != nil {
+			return Instruction{}, err
+		}
+		data, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return Instruction{}, err
+		}
+		return Instruction{op, data}, nil
+	}
+	// otherwise, we just need to parse the instruction
+	op, err := ParseOperand(parts[0])
+	if err != nil {
+		return Instruction{}, err
+	}
 
-  return Instruction{op, 0}, nil
+	return Instruction{op, 0}, nil
 }
 
 func ParseOperand(op string) (OP, error) {
-  switch op {
-    case "PUSH":
-      return PUSH, nil
-    case "POP":
-      return POP, nil
-    case "ADD":
-      return ADD, nil
-    case "SUB":
-      return SUB, nil
-    case "MUL":
-      return MUL, nil
-    case "DIV":
-      return DIV, nil
-    case "MOD":
-      return MOD, nil
-    case "DUMP":
-      return DUMP, nil
-    default:
-      return OP(0), fmt.Errorf("Invalid instruction: %s", op)
-  }
+	switch op {
+	case "PUSH":
+		return PUSH, nil
+	case "POP":
+		return POP, nil
+	case "ADD":
+		return ADD, nil
+	case "SUB":
+		return SUB, nil
+	case "MUL":
+		return MUL, nil
+	case "DIV":
+		return DIV, nil
+	case "MOD":
+		return MOD, nil
+	case "DUMP":
+		return DUMP, nil
+	default:
+		return OP(0), fmt.Errorf("Invalid instruction: %s", op)
+	}
 }
